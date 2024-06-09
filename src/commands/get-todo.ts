d323d64f21204793fbedebaeef1305047e0f1c81
Shashank Gupta <shashankgpt270@gmail.com>
2024-06-10 00:51:22 +0530

    variable name fixed

--- src/commands/get-todo.ts
+++ src/commands/get-todo.ts
@@ -3,7 +3,7 @@ import { handleGetTodoCommand } from "../actions/handler";
 
 export default function createTodoCommand(cmd: Command) {
     cmd
-        .command('get-todo [id]')
+        .command('get-todo [count]')
         .description('fetch a todo and display it')
         .action(handleGetTodoCommand);
 }
