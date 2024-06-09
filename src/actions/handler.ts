0eb3911e4b5c806730bf3a6a7808aa8f61064aaf
Shashank Gupta <shashankgpt270@gmail.com>
2024-06-09 22:56:58 +0530

    create display function

--- src/actions/handler.ts
+++ src/actions/handler.ts
@@ -1,19 +1,19 @@
 import { getTodos } from "../request";
 import { getListSchema } from "../schema";
-import { getEvenNumber } from "../util";
+import { getEvenNumber, displayTodos } from "../util";
 
 export async function handleGetTodoCommand(countArg: any) {
   try {
     const count = countArg && Number.parseInt(countArg) ? Number(countArg) : 20;
     const todoList = getEvenNumber(count);
     const todos = await getTodos(todoList);
+
+    // validate the response
     const validateResult = getListSchema.validate(todos);
     if (validateResult.error) {
       throw new Error(validateResult.error.message);
     }
-    todos.forEach(({ id, title, completed }) => {
-      console.log(`Id:${id} ${completed ? "[x]" : "[ ]"} - ${title}`);
-    });
+    displayTodos(todos);
   } catch (error) {
     console.error(error);
   }
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,7 +1,15 @@
+import { Todo } from "../type";
+
 export function getEvenNumber(count: number): number[] {
     const even20 = [];
     for (let i = 1; i <= count; i++) {
         even20.push(i * 2);
     }
     return even20;
+}
+
+export function displayTodos(todos: Todo[]): void {
+    todos.forEach(({ id, title, completed }) => {
+        console.log(`Id:${id} ${completed ? "[x]" : "[ ]"} - ${title}`);
+    });
 }
